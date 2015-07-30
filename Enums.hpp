9e793e150cb0531f788efa30d212d478c4692cc2
dzejrou <dzejrou@gmail.com>
2015-07-30 14:03:26 +0200

    created special header for enums, TODO: Move all enums here!

---
+++ Enums.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+enum class ENTITY_STATE
+{
+	NONE, NORMAL 
+};
+
+enum class FACTION
+{
+	FRIENDLY, ENEMY, NEUTRAL
+};
+
+enum class ENTITY_TYPE
+{
+	NONE, WALL
+};
+
+enum class EVENT_TYPE
+{
+	NONE
+};
+
+enum class TASK_TYPE
+{
+	NONE, GOTO
+};
