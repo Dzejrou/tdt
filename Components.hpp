3268773432a0c8e45b04b570c3f70ed2a9e0a0c7
dzejrou <dzejrou@gmail.com>
2015-08-05 13:22:23 +0200

    added more constructor options to components (will be probably used for game saving and loading)

--- Components.hpp
+++ Components.hpp
@@ -34,13 +34,14 @@ struct PhysicsComponent
 {
 	static constexpr int type = 0;
 
-	PhysicsComponent(bool s = false)
-		: position{0, 0, 0}, solid{s}, half_height{false}
+	PhysicsComponent(bool s = false, Ogre::Vector3 pos = Ogre::Vector3{0, 0, 0}, Ogre::Real hh = 0.f)
+		: solid{s}, position{pos}, half_height{hh}
 	{ /* DUMMY BODY */ }
 	PhysicsComponent(const PhysicsComponent&) = default;
+	PhysicsComponent(PhysicsComponent&&) = default;
 
-	Ogre::Vector3 position;
 	bool solid;
+	Ogre::Vector3 position;
 	Ogre::Real half_height;
 };
 
@@ -51,10 +52,12 @@ struct HealthComponent
 {
 	static constexpr int type = 1;
 
-	HealthComponent(std::size_t max = 0, std::size_t reg = 0, std::size_t def = 0)
-		: curr_hp{max}, max_hp{max}, regen{reg}, defense{def}, alive{true}
+	HealthComponent(std::size_t max = 0, std::size_t reg = 0,
+					std::size_t def = 0, bool a = true)
+		: curr_hp{max}, max_hp{max}, regen{reg}, defense{def}, alive{a}
 	{ /* DUMMY BODY */ }
 	HealthComponent(const HealthComponent&) = default;
+	HealthComponent(HealthComponent&&) = default;
 
 	std::size_t curr_hp;
 	std::size_t max_hp;
@@ -71,10 +74,13 @@ struct AIComponent
 {
 	static constexpr int type = 2;
 
-	AIComponent(const std::string& s = "ERROR", FACTION f = FACTION::NEUTRAL)
-		: blueprint{s}, state{ENTITY_STATE::NORMAL}, faction(f)
+	AIComponent(const std::string& s = "ERROR",
+				FACTION f = FACTION::NEUTRAL,
+				ENTITY_STATE st = ENTITY_STATE::NORMAL)
+		: blueprint{s}, state{st}, faction(f)
 	{ /* DUMMY BODY */ }
 	AIComponent(const AIComponent&) = default;
+	AIComponent(AIComponent&&) = default;
 
 	std::string blueprint;
 	ENTITY_STATE state;
@@ -88,17 +94,19 @@ struct GraphicsComponent
 {
 	static constexpr int type = 3;
 
-	GraphicsComponent(const std::string& me = "ogrehead.mesh", const std::string& ma = "Ogre")
-		: mesh{me}, material{ma}, visible{true}, node{nullptr}, entity{nullptr}
+	GraphicsComponent(const std::string& me = "ogrehead.mesh",
+					  const std::string& ma = "Ogre", bool v = true)
+		: mesh{me}, material{ma}, visible{v}, node{nullptr}, entity{nullptr}
 	{ /* DUMMY BODY */ }
-	GraphicsComponent(const GraphicsComponent&) = default;
+	GraphicsComponent(const GraphicsComponent&) = delete;
+	GraphicsComponent(GraphicsComponent&&) = delete;
 
 	std::string mesh;
 	std::string material;
 	bool visible;
 	Ogre::SceneNode* node;
 	Ogre::Entity* entity;
-	// TODO: Animation + set_animation etc. in a system.
+	// TODO: Animation + set_animation etc. in a system. Possibly a new component?
 };
 
 /**
@@ -110,11 +118,13 @@ struct MovementComponent
 {
 	static constexpr int type = 4;
 
-	MovementComponent(Ogre::Real speed = 0.f)
-		: movement_vector{0, 0, 0}, speed_modifier{speed},
-		  moving{false}
+	MovementComponent(Ogre::Vector3 mov_vec = Ogre::Vector3{0, 0, 0},
+					  Ogre::Real speed = 0.f, bool mov = false)
+		: movement_vector{mov_vec}, speed_modifier{speed},
+		  moving{mov}
 	{ /* DUMMY BODY */ }
 	MovementComponent(const MovementComponent&) = default;
+	MovementComponent(MovementComponent&&) = default;
 
 	Ogre::Vector3 movement_vector;
 	Ogre::Real speed_modifier;
@@ -129,10 +139,13 @@ struct CombatComponent
 {
 	static constexpr int type = 5;
 
-	CombatComponent(std::size_t r = 0, std::size_t mi = 0, std::size_t ma = 0, int a1 = 0, int a2 = 0, float a2_chance = 0.f)
-		: range{r}, min_dmg{mi}, max_dmg{ma}, atk_1((AttackType)a1), atk_2((AttackType)a2), atk2_chance{a2_chance}
+	CombatComponent(std::size_t r = 0, std::size_t mi = 0, std::size_t ma = 0,
+					int a1 = 0, int a2 = 0, float a2_chance = 0.f)
+		: range{r}, min_dmg{mi}, max_dmg{ma}, atk_1((AttackType)a1),
+		  atk_2((AttackType)a2), atk2_chance{a2_chance}
 	{ /* DUMMY BODY */ }
 	CombatComponent(const CombatComponent&) = default;
+	CombatComponent(CombatComponent&&) = default;
 
 	std::size_t range;
 	std::size_t min_dmg;
@@ -173,6 +186,7 @@ struct InputComponent
 		: input_handler{handler}
 	{ /* DUMMY BODY */ }
 	InputComponent(const InputComponent&) = default;
+	InputComponent(InputComponent&&) = default;
 
 	std::string input_handler;
 };
@@ -218,24 +232,27 @@ struct ProductionComponent
 struct GridNodeComponent
 {
 	static constexpr int type = 12;
+	static constexpr std::size_t neighbour_count = 8;
 
-	GridNodeComponent()
-		: neighbours{}, free{true}, x{0}, y{0}, resident{ENTITY_TYPE::NONE}
+	GridNodeComponent(std::array<std::size_t, neighbour_count> neigh = std::array<std::size_t, neighbour_count>{},
+					  bool f = true, std::size_t pos_x = 0, std::size_t pos_y = 0, std::size_t res = Component::NO_ENTITY)
+		: neighbours(neigh), free{f}, x{pos_x}, y{pos_y}, resident{res}
 	{
 		for(std::size_t i = 0; i < neighbours.size(); ++i)
 			neighbours[i] = Component::NO_ENTITY;
 	}
 	GridNodeComponent(const GridNodeComponent&) = default;
+	GridNodeComponent(GridNodeComponent&&) = default;
 
 	/**
 	 * Note: For more versatility of the game engine, using
 	 * a std::vector instead would allow for non grid-like
 	 * pathfinding graphs.
 	 */
-	std::array<std::size_t, 8> neighbours;
+	std::array<std::size_t, neighbour_count> neighbours;
 	bool free;
 	std::size_t x, y; // Position in the grid.
-	ENTITY_TYPE resident;
+	std::size_t resident;
 };
 
 /**
@@ -250,6 +267,7 @@ struct GridLineComponent
 		: start_id{start}, end_id{end}
 	{ /* DUMMY BODY */ }
 	GridLineComponent(const GridLineComponent&) = default;
+	GridLineComponent(GridLineComponent&&) = default;
 
 	std::size_t start_id, end_id;
 };
@@ -261,10 +279,12 @@ struct PathfindingComponent
 {
 	static constexpr int type = 14;
 
-	PathfindingComponent(const std::string& cost = "ERROR")
-		: target_id{0}, last_id{0}, path_queue{}, blueprint{cost}
+	PathfindingComponent(const std::string& cost = "ERROR", std::size_t tar = 0,
+						 std::size_t last = 0)
+		: target_id{tar}, last_id{last}, path_queue{}, blueprint{cost}
 	{ /* DUMMY BODY */ }
 	PathfindingComponent(const PathfindingComponent&) = default;
+	PathfindingComponent(PathfindingComponent&&) = default;
 	
 	std::size_t target_id, last_id;
 	std::deque<std::size_t> path_queue;
@@ -284,6 +304,7 @@ struct TaskComponent
 		: task_type{t_type}, source{source_id}, target{target_id}
 	{ /* DUMMY BODY */ }
 	TaskComponent(const TaskComponent&) = default;
+	TaskComponent(TaskComponent&&) = default;
 
 	TASK_TYPE task_type;
 	std::size_t source, target;
@@ -296,9 +317,9 @@ struct TaskHandlerComponent
 {
 	static constexpr int type = 16;
 
-	TaskHandlerComponent()
-		: curr_task{Component::NO_ENTITY}, possible_tasks{}, task_queue{},
-		  busy{false}
+	TaskHandlerComponent(std::size_t curr = Component::NO_ENTITY)
+		: curr_task{curr}, possible_tasks{}, task_queue{},
+		  busy{curr == Component::NO_ENTITY}
 	{  /* DUMMY BODY */ }
 	TaskHandlerComponent(const TaskHandlerComponent&) = default;
 
