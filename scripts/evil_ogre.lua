5d299874da09d37320540c6ca144cdb07ae3be05
dzejrou <dzejrou@gmail.com>
2015-08-17 16:02:08 +0200

    created a second testing entity, evil_ogre, which has enemy faction and can be used to test the combat system
    also added a new update function to both ogre and evil_ogre, which finds
    the closest enemy in sight and attacks, or finds the closest enemy and
    goes to him to attack him

---
+++ scripts/evil_ogre.lua
@@ -0,0 +1,110 @@
+-- Testing Ogre entity.
+ogre = {
+	components = {
+		-- Enums contained in core_utils.lua
+		game.enum.component.physics,
+		game.enum.component.health,
+		game.enum.component.ai,
+		game.enum.component.graphics,
+		game.enum.component.movement,
+		game.enum.component.combat,
+		game.enum.component.event,
+		game.enum.component.pathfinding,
+		game.enum.component.task_handler
+	},
+
+	PhysicsComponent = {
+		solid = false
+	},
+
+	HealthComponent = {
+		max_hp = 1000,
+		regen = 1,
+		defense = 8
+	},
+
+	AIComponent = {
+		blueprint = "ogre",
+		faction = game.enum.faction.enemy
+	},
+
+	GraphicsComponent = {
+		mesh = "ogrehead.mesh",
+		material = "NO_MAT", -- Material is in mesh.
+		visible = true,
+		manual_scaling = false
+	},
+
+	MovementComponent = {
+		speed_modifier = 0.5,
+	},
+
+	CombatComponent = {
+		range = 800,
+		min_dmg = 15,
+		max_dmg = 30,
+		type = game.enum.atk_type.ranged,
+		cooldown = 3
+	},
+
+	EventComponent = {
+		curr_event_progress = 0,
+		curr_event_length = 0,
+		curr_event_type = 0
+	},
+
+	PathfindingComponent = {
+		blueprint = "ogre"
+	},
+
+	TaskHandlerComponent = {
+		possible_tasks = {
+			game.enum.task.go_to,
+			game.enum.task.go_near,
+			game.enum.task.go_kill,
+			game.enum.task.kill,
+			game.enum.task.get_in_range
+		}
+	},
+
+	InputComponent = {
+		-- Won't get loaded, but serves as a blueprint if needed.
+		input_handler = "ogre_handler"
+	},
+
+	init = function(id)
+		evil_ogre.stats.current_amount = evil_ogre.stats.current_amount + 1
+		evil_ogre.stats.overall_amount = evil_ogre.stats.overall_amount + 1
+	end,
+
+	update = function(id)
+		enemy = game.closest_enemy_in_sight(id)
+		if enemy ~= game.const.no_ent then
+			task = game.create_task(enemy, game.enum.task.go_kill)
+			game.add_task(id, task)
+			return
+		end
+
+		-- If failed, try enemy not in sight.
+		enemy = game.closest_enemy(id)
+		if enemy ~= game.const.no_ent then
+			task = game.create_task(enemy, game.enum.task.go_kill)
+			game.add_task(id, task)
+			return
+		end
+	end,
+
+	finnish = function(id)
+		evil_ogre.stats.current_amount = evil_ogre.stats.current_amount - 1
+	end,
+
+	stats = {
+		current_amount = 0,
+		overall_amount = 0,
+		default_blueprint = "evil_ogre"
+	}
+}
+
+if game then
+	game.register_entity("evil_ogre")
+end
