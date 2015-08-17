397afd4522a126c6ab56f06b75c9cff80d42528d
dzejrou <dzejrou@gmail.com>
2015-08-17 16:00:06 +0200

    created a script representing a basic ranged projectile, that keeps homing on it's target and then deals given amount of damage

---
+++ scripts/basic_projectile.lua
@@ -0,0 +1,36 @@
+basic_projectile = {
+	components = {
+		game.enum.component.physics,
+		game.enum.component.graphics,
+		game.enum.component.movement,
+		game.enum.component.homing
+	},
+
+	PhysicsComponent = {
+		solid = false
+	},
+
+	GraphicsComponent = {
+		mesh = "sphere.mesh",
+		material = "colour/red",
+		visible = true,
+		manual_scaling = true,
+		scale_x = 10,
+		scale_y = 10,
+		scale_z = 10
+	},
+
+	MovementComponent = {
+		speed_modifier = 0.7
+	},
+
+	HomingComponent = {
+		source = game.const.no_ent,
+		target = game.const.no_ent,
+		damage = 0
+	}
+}
+
+if game then
+	game.register_entity("basic_projectile")
+end
