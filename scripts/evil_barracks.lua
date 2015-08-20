86945e55a168ad9dd861fbe13083986141d8e9f9
dzejrou <dzejrou@gmail.com>
2015-08-20 17:18:05 +0200

    added evil barracks, which spawn evil ogres

---
+++ scripts/evil_barracks.lua
@@ -0,0 +1,36 @@
+evil_barracks = {
+	components = {
+		game.enum.component.physics,
+		game.enum.component.graphics,
+		game.enum.component.production,
+		game.enum.component.structure
+	},
+
+	PhysicsComponent = {
+		solid = false
+	},
+
+	GraphicsComponent = {
+		mesh = "cube.mesh",
+		material = "colour/red",
+		visible = true,
+		manual_scaling = true,
+		scale_x = 50,
+		scale_y = 10,
+		scale_z = 50,
+		query_flags = game.enum.entity_type.building
+	},
+
+	ProductionComponent = {
+		blueprint = "evil_ogre",
+		limit = 1,
+		cooldown = 5
+	},
+
+	StructureComponent = {
+		radius = 0
+	}
+}
+if game then
+	game.register_entity("evil_barracks")
+end
