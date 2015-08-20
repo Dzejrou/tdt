fc11f7754e5ef7b5d2877338f5a3c0bd02a4a441
dzejrou <dzejrou@gmail.com>
2015-08-20 17:35:26 +0200

    added documenting comments

--- ProductionSystem.hpp
+++ ProductionSystem.hpp
@@ -6,15 +6,46 @@
 #include "EntitySystem.hpp"
 #include "GridSystem.hpp"
 
+/**
+ * System taking care of entities spawned by buildings and the spawn counts
+ * allowing for a constant amount of entities (related to the number of buildings
+ * spawning entities of that blueprint table).
+ */
 class ProductionSystem : public System
 {
 	public:
+		/**
+		 * Constructor.
+		 * Param: Reference to the game's entity system.
+		 * Param: Reference to the game's grid system (spawn positioning).
+		 */
 		ProductionSystem(EntitySystem&, GridSystem&);
+
+		/**
+		 * Destructor.
+		 */
 		~ProductionSystem() {}
 
+		/**
+		 * Brief: Iterates over all buildings and when possible, spawns new entities.
+		 * Param: Time since last frame.
+		 */
 		void update(Ogre::Real) override;
+
+		/**
+		 * Brief: Spawns a single entity created by a building.
+		 * Param: ID of the building.
+		 * Param: Name of the blueprint table of the spawned entity.
+		 */
 		void spawn_entity(std::size_t, const std::string&);
 	private:
+		/**
+		 * Reference to the game's entity system (component retrieval).
+		 */
 		EntitySystem& entities_;
+
+		/**
+		 * Reference to the game's grid system (spawn positioning).
+		 */
 		GridSystem& grid_;
 };
