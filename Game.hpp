9028b8c79173b3cf0aabb826d441350c8469e18c
dzejrou <dzejrou@gmail.com>
2015-08-21 18:58:45 +0200

    moved all lua x C++ interface stuff into the new LuaInterface class
    this will make Game.cpp more readable and provides a single static class
    to handle Lua - C++ interaction

--- Game.hpp
+++ Game.hpp
@@ -24,11 +24,13 @@
 #include "CombatSystem.hpp"
 #include "ProductionSystem.hpp"
 #include "Enums.hpp"
+#include "LuaInterface.hpp"
 
 class Game : public Ogre::FrameListener, public OIS::KeyListener,
 			 public OIS::MouseListener, public Ogre::WindowEventListener
 {
 	friend class GameSerializer;
+	friend class LuaInterface;
 	public:
 		/**
 		 * Constructor.
@@ -76,7 +78,6 @@ class Game : public Ogre::FrameListener, public OIS::KeyListener,
 		void ogre_init();
 		void ois_init();
 		void level_init();
-		void lua_init();
 		void cegui_init();
 
 		/**
@@ -183,166 +184,8 @@ class Game : public Ogre::FrameListener, public OIS::KeyListener,
 		std::unique_ptr<SelectionBox> selection_box_;
 
 		/**
-		 *
+		 * A gui window that allows to place entities selected from a menu.
+		 * (TODO: Allow to create/modify entities.)
 		 */
 		std::unique_ptr<EntityCreator> entity_creator_;
-
-		/**
-		 * These methods are to be used from Lua, note that Lua can register only
-		 * static functions, so a static pointer to the instance of this game is created in
-		 * the game's constructor and used in these static functions.
-		 */
-		static Game* lua_this;
-
-		// Core functions.
-		static int lua_get_avg_fps(lpp::Script::state);
-		static int lua_get_fps(lpp::Script::state);
-		static int lua_print(lpp::Script::state);
-		static int lua_set_game_state(lpp::Script::state);
-		static int lua_toggle_bounding_boxes(lpp::Script::state);
-		static int lua_toggle_camera_free_mode(lpp::Script::state);
-		static int lua_toggle_entity_creator(lpp::Script::state);
-		static int lua_list_selected(lpp::Script::state);
-		static int lua_destroy_selected(lpp::Script::state);
-		static int lua_list_components_of(lpp::Script::state);
-		static int lua_load(lpp::Script::state);
-		static int lua_reload_all(lpp::Script::state);
-		static int lua_save_game(lpp::Script::state);
-		static int lua_load_game(lpp::Script::state);
-
-		// Ogre related functions. TODO: Create a special system for this!
-		static int lua_set_mesh(lpp::Script::state);
-		static int lua_set_material(lpp::Script::state);
-		static int lua_set_visible(lpp::Script::state);
-		static int lua_set_manual_scaling(lpp::Script::state);
-		static int lua_set_scale(lpp::Script::state);
-
-		// Entity system.
-		static int lua_create_entity(lpp::Script::state);
-		static int lua_destroy_entity(lpp::Script::state);
-		static int lua_add_component(lpp::Script::state);
-		static int lua_delete_component(lpp::Script::state);
-		static int lua_init_graphics_component(lpp::Script::state);
-		static int lua_list_entity_tables(lpp::Script::state);
-		static int lua_place_entity(lpp::Script::state);
-		static int lua_register_entity(lpp::Script::state);
-
-		// Movement system.
-		static int lua_move_to(lpp::Script::state);
-		static int lua_move(lpp::Script::state);
-		static int lua_rotate(lpp::Script::state);
-		static int lua_is_moving(lpp::Script::state);
-		static int lua_is_solid(lpp::Script::state);
-		static int lua_can_move_to(lpp::Script::state);
-		static int lua_collide(lpp::Script::state);
-		static int lua_get_distance(lpp::Script::state);
-		static int lua_get_position(lpp::Script::state);
-		static int lua_get_speed_modifier(lpp::Script::state);
-		static int lua_set_speed_modifier(lpp::Script::state);
-		static int lua_enemy_in_radius(lpp::Script::state);
-		static int lua_dir_to_closest_enemy(lpp::Script::state);
-		static int lua_dir_to_closest_enemy_in_radius(lpp::Script::state);
-		static int lua_dir_to_enemy(lpp::Script::state);
-		static int lua_get_dir(lpp::Script::state);
-		static int lua_get_dir_back(lpp::Script::state);
-		static int lua_get_dir_left(lpp::Script::state);
-		static int lua_get_dir_right(lpp::Script::state);
-		static int lua_get_angle(lpp::Script::state);
-		static int lua_get_angle_between(lpp::Script::state);
-		static int lua_look_at(lpp::Script::state);
-		static int lua_set_solid(lpp::Script::state);
-		static int lua_set_half_height(lpp::Script::state);
-
-		// Health system.
-		static int lua_get_health(lpp::Script::state);
-		static int lua_add_health(lpp::Script::state);
-		static int lua_sub_health(lpp::Script::state);
-		static int lua_heal(lpp::Script::state);
-		static int lua_buff(lpp::Script::state);
-		static int lua_get_defense(lpp::Script::state);
-		static int lua_add_defense(lpp::Script::state);
-		static int lua_sub_defense(lpp::Script::state);
-		static int lua_set_regen(lpp::Script::state);
-		static int lua_set_alive(lpp::Script::state);
-
-		// AI system.
-		static int lua_is_friendly(lpp::Script::state);
-		static int lua_is_neutral(lpp::Script::state);
-		static int lua_is_inanimate(lpp::Script::state);
-		static int lua_get_blueprint(lpp::Script::state);
-		static int lua_get_state(lpp::Script::state);
-		static int lua_get_faction(lpp::Script::state);
-		static int lua_set_blueprint(lpp::Script::state);
-		static int lua_set_state(lpp::Script::state);
-		static int lua_set_faction(lpp::Script::state);
-
-		// Input system.
-		static int lua_set_input_handler(lpp::Script::state);
-		static int lua_toggle_first_person(lpp::Script::state);
-
-		// Grid system.
-		static int lua_add_node(lpp::Script::state);
-		static int lua_get_node(lpp::Script::state);
-		static int lua_get_node_from_position(lpp::Script::state);
-		static int lua_create_grid_graphics(lpp::Script::state);
-		static int lua_delete_grid_graphics(lpp::Script::state);
-		static int lua_toggle_grid_visible(lpp::Script::state);
-		static int lua_is_free(lpp::Script::state);
-		static int lua_set_free(lpp::Script::state);
-		static int lua_set_free_selected(lpp::Script::state);
-		static int lua_pathfind(lpp::Script::state);
-		static int lua_clear_path_colour(lpp::Script::state);
-		static int lua_set_pathfinding_blueprint(lpp::Script::state);
-		static int lua_create_graph(lpp::Script::state);
-		static int lua_set_resident(lpp::Script::state);
-		static int lua_get_resident(lpp::Script::state);
-		static int lua_add_residences(lpp::Script::state);
-		static int lua_add_residence(lpp::Script::state);
-		static int lua_set_radius(lpp::Script::state);
-		static int lua_set_walk_through(lpp::Script::state);
-
-		// Task system.
-		static int lua_add_task(lpp::Script::state);
-		static int lua_cancel_task(lpp::Script::state);
-		static int lua_create_task(lpp::Script::state);
-		static int lua_list_tasks_of(lpp::Script::state);
-		static int lua_task_possible(lpp::Script::state);
-		static int lua_clear_task_queue(lpp::Script::state);
-		static int lua_set_task_source(lpp::Script::state);
-		static int lua_set_task_target(lpp::Script::state);
-		static int lua_set_task_type(lpp::Script::state);
-		static int lua_add_possible_task(lpp::Script::state);
-		static int lua_delete_possible_task(lpp::Script::state);
-
-		// Combat system.
-		static int lua_set_range(lpp::Script::state);
-		static int lua_get_range(lpp::Script::state);
-		static int lua_set_dmg_range(lpp::Script::state);
-		static int lua_get_dmg_range(lpp::Script::state);
-		static int lua_set_cooldown(lpp::Script::state);
-		static int lua_get_cooldown(lpp::Script::state);
-		static int lua_set_atk_type(lpp::Script::state);
-		static int lua_get_atk_type(lpp::Script::state);
-		static int lua_set_homing_source(lpp::Script::state);
-		static int lua_set_homing_target(lpp::Script::state);
-		static int lua_set_homing_dmg(lpp::Script::state);
-		static int lua_closest_enemy_in_sight(lpp::Script::state);
-		static int lua_closest_friendly_in_sight(lpp::Script::state);
-		static int lua_closest_enemy(lpp::Script::state);
-		static int lua_closest_friendly(lpp::Script::state);
-
-		// Production system.
-		static int lua_set_production_blueprint(lpp::Script::state);
-		static int lua_get_production_blueprint(lpp::Script::state);
-		static int lua_set_production_limit(lpp::Script::state);
-		static int lua_get_production_limit(lpp::Script::state);
-		static int lua_set_production_cooldown(lpp::Script::state);
-		static int lua_get_production_cooldown(lpp::Script::state);
-		static int lua_set_production_progress(lpp::Script::state);
-		static int lua_get_production_progress(lpp::Script::state);
-		static int lua_set_production_count(lpp::Script::state);
-		static int lua_get_production_count(lpp::Script::state);
-		static int lua_set_producer(lpp::Script::state);
-		static int lua_get_producer(lpp::Script::state);
-		static int lua_instant_production(lpp::Script::state);
 };
