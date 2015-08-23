4462ef3fa0dfa98e0baf3a8c3188af42a8c66ee4
dzejrou <dzejrou@gmail.com>
2015-08-23 16:49:25 +0200

    created combat helper

---
+++ CombatHelper.cpp
@@ -0,0 +1,73 @@
+#include "CombatHelper.hpp"
+
+void CombatHelper::set_range(EntitySystem& ents, std::size_t id, Ogre::Real range)
+{
+	auto comp = ents.get_component<CombatComponent>(id);
+	if(comp)
+		comp->range = range;
+}
+
+Ogre::Real CombatHelper::get_range(EntitySystem& ents, std::size_t id)
+{
+	auto comp = ents.get_component<CombatComponent>(id);
+	if(comp)
+		return comp->range;
+	else
+		return 0;
+}
+
+void CombatHelper::set_dmg_range(EntitySystem& ents, std::size_t id, std::size_t min, std::size_t max)
+{
+	auto comp = ents.get_component<CombatComponent>(id);
+	if(comp)
+	{
+		comp->min_dmg = min;
+		comp->max_dmg = max;
+	}
+}
+
+std::tuple<std::size_t, std::size_t> CombatHelper::get_dmg_range(EntitySystem& ents, std::size_t id)
+{
+	auto comp = ents.get_component<CombatComponent>(id);
+	if(comp)
+		return std::make_tuple(comp->min_dmg, comp->max_dmg);
+	else
+		return std::tuple<std::size_t, std::size_t>{};
+}
+
+std::size_t CombatHelper::get_dmg(EntitySystem& ents, std::size_t min, std::size_t max)
+{
+	return rd_dist_(rd_gen_) % max + min;
+}
+
+void CombatHelper::set_cooldown(EntitySystem& ents, std::size_t id, Ogre::Real cd)
+{
+	auto comp = ents.get_component<CombatComponent>(id);
+	if(comp)
+		comp->cooldown = cd;
+}
+
+Ogre::Real CombatHelper::get_cooldown(EntitySystem& ents, std::size_t id)
+{
+	auto comp = ents.get_component<CombatComponent>(id);
+	if(comp)
+		return comp->cooldown;
+	else
+		return 0;
+}
+
+void CombatHelper::set_atk_type(EntitySystem& ents, std::size_t id, ATTACK_TYPE type)
+{
+	auto comp = ents.get_component<CombatComponent>(id);
+	if(comp)
+		comp->atk_type = type;
+}
+
+ATTACK_TYPE CombatHelper::get_atk_type(EntitySystem& ents, std::size_t id)
+{
+	auto comp = ents.get_component<CombatComponent>(id);
+	if(comp)
+		return comp->atk_type;
+	else
+		return ATTACK_TYPE::NONE;
+}
