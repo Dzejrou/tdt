890e2749fdf890e8f846748876d4224b95f367ff
dzejrou <dzejrou@gmail.com>
2015-08-23 19:22:05 +0200

    created task handler helper

---
+++ TaskHandlerHelper.cpp
@@ -0,0 +1,53 @@
+#include "TaskHandlerHelper.hpp"
+#include "Components.hpp"
+#include "EntitySystem.hpp"
+
+std::deque<std::size_t> TaskHandlerHelper::get_task_queue(EntitySystem& ents, std::size_t id)
+{
+	auto comp = ents.get_component<TaskHandlerComponent>(id);
+	if(comp)
+		return comp->task_queue;
+	else
+		return std::deque<std::size_t>{};
+}
+
+bool TaskHandlerHelper::task_possible(EntitySystem& ents, std::size_t ent_id, std::size_t task_id)
+{
+	auto comp1 = ents.get_component<TaskHandlerComponent>(ent_id);
+	auto comp2 = ents.get_component<TaskComponent>(task_id);
+	if(comp1 && comp2)
+	{
+		return comp1->possible_tasks.test((int)comp2->task_type);
+	}
+	else
+		return false;
+}
+
+void TaskHandlerHelper::clear_task_queue(EntitySystem& ents, std::size_t id)
+{
+	auto comp = ents.get_component<TaskHandlerComponent>(id);
+	if(comp)
+	{
+		auto& task_queue = comp->task_queue;
+		for(auto& task : task_queue)
+		{
+			if(ents.has_component<TaskComponent>(task))
+				ents.destroy_entity(task);
+		}
+		task_queue.clear();
+	}
+}
+
+void TaskHandlerHelper::add_possible_task(EntitySystem& ents, std::size_t id, TASK_TYPE type)
+{
+	auto comp = ents.get_component<TaskHandlerComponent>(id);
+	if(comp)
+		comp->possible_tasks.set((int)type);
+}
+
+void TaskHandlerHelper::delete_possible_task(EntitySystem& ents, std::size_t id, TASK_TYPE type)
+{
+	auto comp = ents.get_component<TaskHandlerComponent>(id);
+	if(comp)
+		comp->possible_tasks.set((int)type, false);
+}
---
+++ TaskHandlerHelper.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+class EntitySystem;
+
+namespace TaskHandlerHelper
+{
+	/**
+	 * Brief: Returns a reference to the task queue of a given entity.
+	 * Param: Reference to the entity system containing needed components.
+	 * Param: ID of the entity.
+	 */
+	std::deque<std::size_t> get_task_queue(EntitySystem&, std::size_t);
+
+	/**
+	 * Brief: Checks whether an entity can accept and complete a given task.
+	 * Param: Reference to the entity system containing needed components.
+	 * Param: ID of the entity.
+	 * Param: ID of the task.
+	 */
+	bool task_possible(EntitySystem&, std::size_t, std::size_t);
+
+	/**
+	 * Brief: Cancels all tasks in a given entity's task queue.
+	 * Param: Reference to the entity system containing needed components.
+	 * Param: ID of the entity.
+	 */
+	void clear_task_queue(EntitySystem&, std::size_t);
+
+	/**
+	 * Brief: Marks a given entity as available to accept tasks of a given task type.
+	 * Param: Reference to the entity system containing needed components.
+	 * Param: ID of the entity.
+	 * Param: Type of the task to be added.
+	 */
+	void add_possible_task(EntitySystem&, std::size_t, TASK_TYPE);
+
+	/**
+	 * Brief: Marks a given entity as unavailable to accept tasks of a given task type.
+	 * Param: Reference to the entity system containing needed components.
+	 * Param: ID of the entity.
+	 * Param: Type of the task to be deleted.
+	 */
+	void delete_possible_task(EntitySystem&, std::size_t, TASK_TYPE);
+}
