d74efa90f4f528fb4ddb2dd8ed7ff59694614771
dzejrou <dzejrou@gmail.com>
2015-08-23 15:43:50 +0200

    removed the methods that were move to HealthHelper

--- HealthSystem.cpp
+++ HealthSystem.cpp
@@ -1,7 +1,7 @@
 #include "HealthSystem.hpp"
 
 HealthSystem::HealthSystem(EntitySystem& ent)
-	: entities_{ent}, regen_timer_{0}, regen_period_{1000} // TODO: Experiment with regen period!
+	: entities_{ent}, regen_timer_{0}, regen_period_{1000.f} // TODO: Time period in a config!
 { /* DUMMY BODY */ }
 
 void HealthSystem::update(Ogre::Real delta)
@@ -10,7 +10,7 @@ void HealthSystem::update(Ogre::Real delta)
 	for(auto& ent : entities_.get_component_container<HealthComponent>())
 	{
 		if(!ent.second.alive)
-		{ // Entity died.
+		{
 			auto product_component = entities_.get_component<ProductComponent>(ent.first);
 			if(product_component)
 			{
@@ -24,13 +24,8 @@ void HealthSystem::update(Ogre::Real delta)
 			entities_.destroy_entity(ent.first);
 		}
 		else if(regen_)
-			add_health(ent.first, ent.second.regen);
+			HealthHelper::add_health(entities_, ent.first, ent.second.regen);
 	}
-}
-
-bool HealthSystem::is_valid(std::size_t id) const
-{
-	return entities_.has_component<HealthComponent>(id);
 }
 
 void HealthSystem::update_regen(Ogre::Real delta)
@@ -50,103 +45,8 @@ void HealthSystem::update_regen(Ogre::Real delta)
 		regen_timer_ += delta;
 }
 
-std::size_t HealthSystem::get_health(std::size_t id) const
 {
-	auto comp = entities_.get_component<HealthComponent>(id);
-	if(comp)
-		return comp->curr_hp;
-	else
-		return 0;
 }
 
-void HealthSystem::add_health(std::size_t id, std::size_t val)
 {
-	auto comp = entities_.get_component<HealthComponent>(id);
-	if(comp)
-	{
-		std::size_t curr = comp->curr_hp;
-		std::size_t max = comp->max_hp;
-
-		if(curr + val > max)
-			val = max - curr;
-		comp->curr_hp += val;
-	}
-}
-
-void HealthSystem::sub_health(std::size_t id, std::size_t val, bool ignore_defense)
-{
-	auto comp = entities_.get_component<HealthComponent>(id);
-	if(comp)
-	{
-		if(!ignore_defense)
-			val -= comp->defense;
-
-		if(comp->curr_hp <= val)
-		{
-			comp->curr_hp = 0;
-			comp->alive = false;
-		}
-		else
-			comp->curr_hp -= val;
-
-	}
-}
-
-void HealthSystem::heal(std::size_t id)
-{
-	auto comp = entities_.get_component<HealthComponent>(id);
-	if(comp)
-		comp->curr_hp = comp->max_hp;
-}
-
-void HealthSystem::buff(std::size_t id, std::size_t val)
-{
-	auto comp = entities_.get_component<HealthComponent>(id);
-	if(comp)
-	{
-		comp->curr_hp += val;
-		comp->max_hp += val;
-	}
-}
-
-void HealthSystem::set_regen(std::size_t id, std::size_t regen)
-{
-	auto comp = entities_.get_component<HealthComponent>(id);
-	if(comp)
-		comp->regen = regen;
-}
-
-void HealthSystem::set_alive(std::size_t id, bool alive)
-{
-	auto comp = entities_.get_component<HealthComponent>(id);
-	if(comp)
-		comp->alive = alive;
-}
-
-std::size_t HealthSystem::get_defense(std::size_t id) const
-{
-	auto comp = entities_.get_component<HealthComponent>(id);
-	if(comp)
-		return comp->defense;
-	else
-		return 0;
-}
-
-void HealthSystem::add_defense(std::size_t id, std::size_t val)
-{
-	auto comp = entities_.get_component<HealthComponent>(id);
-	if(comp)
-		comp->defense += val;
-}
-
-void HealthSystem::sub_defense(std::size_t id, std::size_t val)
-{
-	auto comp = entities_.get_component<HealthComponent>(id);
-	if(comp)
-	{
-		if(comp->defense - val < 0)
-			comp->defense = 0;
-		else
-			comp->defense -= val;
-	}
 }
--- HealthSystem.hpp
+++ HealthSystem.hpp
@@ -5,6 +5,7 @@
 #include "System.hpp"
 #include "EntitySystem.hpp"
 #include "Components.hpp"
+#include "HealthHelper.hpp"
 
 /**
  * System that manages the regeneration and health of entities
@@ -22,21 +23,14 @@ class HealthSystem : public System
 		/**
 		 * Destructor.
 		 */
-		~HealthSystem() {}
+		~HealthSystem() override {}
 
 		/**
 		 * Brief: Updates a the system by checking every valid entity's health (or death) status
 		 *        and applying health regeneration if necessary.
 		 * Param: Time since the last frame.
 		 */
-		void update(Ogre::Real);
-
-		/**
-		 * Brief: Returns true if an entity is valid, false otherwise. (Should be tested before
-		 *        the update call.)
-		 * Param: ID of the entity.
-		 */
-		bool is_valid(std::size_t) const;
+		void update(Ogre::Real) override;
 
 		/**
 		 * Brief: Adds one to the regeneration timer, simulating continuous regeration, should
@@ -46,76 +40,13 @@ class HealthSystem : public System
 		void update_regen(Ogre::Real);
 
 		/**
-		 * Brief: Returns the current health amount of a given entity.
-		 * Param: ID of the entity.
+		 * Brief: Sets the amount of time it takes for one regeneration
+		 *        tick to happen (in seconds).
+		 * Param: The new regen period.
 		 */
-		std::size_t get_health(std::size_t) const;
 
 		/**
-		 * Brief: Increases the current health amount of an entity by a given amount
-		 *        up to the maximum value stored in it's HealthComponent.
-		 * Param: ID of the entity.
-		 * Param: Amount of health to be added.
-		 * Node: To increase health along with the maximum value, see HealthSystem::buff.
 		 */
-		void add_health(std::size_t, std::size_t);
-
-		/**
-		 * Brief: Subtracts a given amount from the current health of an entity, taking
-		 *        it's defense into account by default.
-		 * Param: ID of the entity.
-		 * Param: Amount of health to be subtracted.
-		 * Param: Optional boolean indicator, if true, the entity's defense will be ignored,
-		 *        otherwise it will be subtracted from the given amount.
-		 */
-		void sub_health(std::size_t, std::size_t, bool = false);
-
-		/**
-		 * Brief: Sets the current health amount of an entity to it's maximum value.
-		 * Param: ID of the entity.
-		 */
-		void heal(std::size_t);
-
-		/**
-		 * Brief: Increases the current and maximum health amount of an entity by a given value.
-		 * Param: ID of the entity.
-		 * Param: Amount of health to be added.
-		 */
-		void buff(std::size_t, std::size_t);
-
-		/**
-		 * Brief: Sets the regeneration value of a given entity.
-		 * Param: ID of the entity.
-		 * Param: The new regen value.
-		 */
-		void set_regen(std::size_t, std::size_t);
-
-		/**
-		 * Brief: Allows to set the health status of an entity without adding/subing health.
-		 * Param: ID of the entity.
-		 * Param: True for "alive" and false for "dead".
-		 */
-		void set_alive(std::size_t, bool);
-
-		/**
-		 * Brief: Returns the defense of a given entity.
-		 * Param: ID of the entity.
-		 */
-		std::size_t get_defense(std::size_t) const;
-
-		/**
-		 * Brief: Increases the defense of an entity by a given amount.
-		 * Param: ID of the entity.
-		 * Param: Amount of defense to be added.
-		 */
-		void add_defense(std::size_t, std::size_t);
-
-		/**
-		 * Brief: Reduces the defense of an entity by a given amount.
-		 * Param: ID of the entity.
-		 * Param: Amounf of defense to be removed.
-		 */
-		void sub_defense(std::size_t, std::size_t);
 	public:
 		/**
 		 * Reference to the game's entity system.
