730218191098b3050b36b97994c4d908264172f0
dzejrou <dzejrou@gmail.com>
2015-08-23 17:15:01 +0200

    created input helper

---
+++ InputHelper.cpp
@@ -0,0 +1,18 @@
+#include "InputHelper.hpp"
+
+void InputHelper::set_input_handler(EntitySystem& ents, std::size_t id, const std::string& handler)
+{
+	auto comp = ents.get_component<InputComponent>(id);
+	if(comp)
+		comp->input_handler = handler;
+}
+
+const std::string & InputHelper::get_input_handler(EntitySystem& ents, std::size_t id)
+{
+	static const std::string NO_HANDLER{"ERROR"};
+	auto comp = ents.get_component<InputComponent>(id);
+	if(comp)
+		return comp->input_handler;
+	else
+		return NO_HANDLER;
+}
---
+++ InputHelper.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "EntitySystem.hpp"
+
+namespace InputHelper
+{
+		/**
+		 * Brief: Changes the name of the table that contains a given
+		 *        entity's input handler.
+		 * Param: Reference to the entity system containing components.
+		 * Param: Name of the new input handler (Lua function).
+		 * Note: The handler recieves the ID of the entity and the key number when it's called, for current
+		 *       keybindings use the game.enum.input Lua table.
+		 */
+		void set_input_handler(EntitySystem&, std::size_t, const std::string&);
+
+		/**
+		 * Brief: Returns name of the table that contains the input handling function
+		 *        of a given entity.
+		 * Param: Reference to the entity system containing components.
+		 * Param: ID of the entity.
+		 */
+		const std::string& get_input_handler(EntitySystem&, std::size_t);
+}
