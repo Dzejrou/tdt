e37045a60f4c2123b4a2646b0f95387350de1309
dzejrou <dzejrou@gmail.com>
2015-08-23 16:24:19 +0200

    added documenting comments

--- AIHelper.hpp
+++ AIHelper.hpp
@@ -4,12 +4,49 @@
 
 namespace AIHelper
 {
+	/**
+	 * Brief: Changes the blueprint table name of a given entity.
+	 * Param: Reference to the entity system containing components.
+	 * Param: ID of the entity.
+	 * Param: Name of the new blueprint table.
+	 */
 	void set_blueprint(EntitySystem&, std::size_t, const std::string&);
+
+	/**
+	 * Brief: Returns name of the blueprint table of a given entity (i.e. the table containing
+	 *        it's init, update and finnish methods).
+	 * Param: Reference to the entity system containing components.
+	 * Param: ID of the entity.
+	 */
 	const std::string& get_blueprint(EntitySystem&, std::size_t);
 
+	/**
+	 * Brief: Changes the FACTION of a given entity.
+	 * Param: Reference to the entity system containing components.
+	 * Param: ID of the entity.
+	 * Param: New FACTION.
+	 */
 	void set_faction(EntitySystem&, std::size_t, FACTION);
+
+	/**
+	 * Brief: Returns the FACTION of a given entity.
+	 * Param: Reference to the entity system containing components.
+	 * Param: ID of the entity.
+	 */
 	FACTION get_faction(EntitySystem&, std::size_t);
 
+	/**
+	 * Brief: Changes the state of a given entity.
+	 * Param: Reference to the entity system containing components.
+	 * Param: ID of the entity.
+	 * Param: New state.
+	 */
 	void set_state(EntitySystem&, std::size_t, ENTITY_STATE);
+
+	/**
+	 * Brief: Returns the state a given entity is in.
+	 * Param: Reference to the entity system containing components.
+	 * Param: ID of the entity.
+	 */
 	ENTITY_STATE get_state(EntitySystem&, std::size_t);
 }
