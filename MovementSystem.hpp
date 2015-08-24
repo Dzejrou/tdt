0e050f29ecb8516d2e2da47c071c68fdb09b9960
dzejrou <dzejrou@gmail.com>
2015-08-24 15:44:01 +0200

    removed the no longer needed enemy finding methods as the only method used for that is now in the combat system

--- MovementSystem.hpp
+++ MovementSystem.hpp
@@ -49,29 +49,6 @@ class MovementSystem : public System
 		 *       increased by the entity's speed modifier. This is not enforced though.
 		 */
 		bool move(std::size_t, Ogre::Vector3);
-
-		/**
-		 * Brief: Returns the ID of the closest enemy in a given radius around a given entity,
-		 *        returns said entity's ID if no enemies are present in the radius.
-		 * Param: ID of the entity.
-		 * Param: The radius value.
-		 */
-		std::size_t enemy_in_radius(std::size_t, Ogre::Real) const;
-
-		/**
-		 * Brief: Returns the ID of an enemy that is closest to a given entity, or said entity's ID
-		 *        if no enemies are around it.
-		 * Param: ID of the entity.
-		 */
-		std::size_t closest_enemy(std::size_t) const;
-
-		/**
-		 * Brief: Returns the direction to the enemy that is closest (with optional maximal distance)
-		 *        to a given entity.
-		 * Param: ID of the entity.
-		 * Param: Optional radius.
-		 */
-		Ogre::Vector3 dir_to_closest_enemy(std::size_t, Ogre::Real = std::numeric_limits<Ogre::Real>::max()) const;
 	private:
 		/**
 		 * Reference to the game's entity system.
