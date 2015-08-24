0d040ffbb160d5c5657c57e772940c2bb70997c3
dzejrou <dzejrou@gmail.com>
2015-08-24 15:41:29 +0200

    added the direction related methods of movement system to the movement helper, this will help to make the input system independent on
    the movement system

--- MovementHelper.cpp
+++ MovementHelper.cpp
@@ -17,3 +17,52 @@ void MovementHelper::set_speed_modifier(EntitySystem& ents, std::size_t id, Ogre
 	if(comp)
 		comp->speed_modifier = val;
 }
+
+Ogre::Vector3 MovementHelper::dir_to(EntitySystem& ents, std::size_t id1, std::size_t id2)
+{
+	auto comp1 = ents.get_component<PhysicsComponent>(id1);
+	auto comp2 = ents.get_component<PhysicsComponent>(id2);
+	if(comp1 && comp2)
+	{
+		auto pos = comp2->position - comp1->position;
+		return pos;
+	}
+	else
+		return Ogre::Vector3{0, 0, 0};
+}
+
+Ogre::Vector3 MovementHelper::get_dir(EntitySystem& ents, std::size_t id)
+{
+	auto comp = ents.get_component<GraphicsComponent>(id);
+	if(comp)
+		return comp->node->getOrientation() * Ogre::Vector3::NEGATIVE_UNIT_Z;
+	else
+		return Ogre::Vector3{0, 0, 0};
+}
+
+Ogre::Vector3 MovementHelper::get_dir_back(EntitySystem& ents, std::size_t id)
+{
+	auto comp = ents.get_component<GraphicsComponent>(id);
+	if(comp)
+		return comp->node->getOrientation() * Ogre::Vector3::UNIT_Z;
+	else
+		return Ogre::Vector3{0, 0, 0};
+}
+
+Ogre::Vector3 MovementHelper::get_dir_left(EntitySystem& ents, std::size_t id)
+{
+	auto comp = ents.get_component<GraphicsComponent>(id);
+	if(comp)
+		return comp->node->getOrientation() * Ogre::Vector3::NEGATIVE_UNIT_X;
+	else
+		return Ogre::Vector3{0, 0, 0};
+}
+
+Ogre::Vector3 MovementHelper::get_dir_right(EntitySystem& ents, std::size_t id)
+{
+	auto comp = ents.get_component<GraphicsComponent>(id);
+	if(comp)
+		return comp->node->getOrientation() * Ogre::Vector3::UNIT_X;
+	else
+		return Ogre::Vector3{0, 0, 0};
+}
--- MovementHelper.hpp
+++ MovementHelper.hpp
@@ -17,4 +17,37 @@ namespace MovementHelper
 	 * Param: New speed value.
 	 */
 	void set_speed_modifier(EntitySystem&, std::size_t, Ogre::Real);
+
+	/**
+	 * Brief: Returns the direction from a given entity to another given entity.
+	 * Param: ID of the first entity.
+	 * Param: ID of the second entity.
+	 */
+	Ogre::Vector3 dir_to(EntitySystem&, std::size_t, std::size_t);
+
+	/**
+	 * Brief: Returns the direction a given entity is facing.
+	 * Param: ID of the entity.
+	 */
+	Ogre::Vector3 get_dir(EntitySystem&, std::size_t);
+
+	/**
+	 * Brief: Returns the opposite direction to the direction a given entity is facing.
+	 * Param: ID of the entity.
+	 */
+	Ogre::Vector3 get_dir_back(EntitySystem&, std::size_t);
+
+	/**
+	 * Brief: Returns the direction perpendicular to the direction a given entity is facing.
+	 *        (To the left.)
+	 * Param: ID of the entity.
+	 */
+	Ogre::Vector3 get_dir_left(EntitySystem&, std::size_t);
+
+	/**
+	 * Brief: Returns the direction perpendicular to the direction a given entity is facing.
+	 *        (To the right.)
+	 * Param: ID of the entity.
+	 */
+	Ogre::Vector3 get_dir_right(EntitySystem&, std::size_t);
 }
