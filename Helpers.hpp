0469615eb86916dbe3d1ae918ef9b12a31a2b54a
dzejrou <dzejrou@gmail.com>
2015-08-24 16:39:45 +0200

    created a common interface for helpers

---
+++ Helpers.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "AIHelper.hpp"
+#include "CombatHelper.hpp"
+#include "GraphicsHelper.hpp"
+#include "HealthHelper.hpp"
+#include "InputHelper.hpp"
+#include "MovementHelper.hpp"
+#include "PathfindingHelper.hpp"
+#include "PhysicsHelper.hpp"
+#include "ProductHelper.hpp"
+#include "ProductionHelper.hpp"
+#include "StructureHelper.hpp"
+#include "TaskHandlerHelper.hpp"
+#include "TaskHelper.hpp"
