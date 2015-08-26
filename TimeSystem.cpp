e792b6769fee35124ce94d2b698aea6f65b34194
dzejrou <dzejrou@gmail.com>
2015-08-26 16:42:46 +0200

    added the ability to speed up/slow down all timers

--- TimeSystem.cpp
+++ TimeSystem.cpp
@@ -1,7 +1,7 @@
 #include "TimeSystem.hpp"
 
 TimeSystem::TimeSystem(EntitySystem& ents)
-	: entities_{ents}
+	: entities_{ents}, time_multiplier_{1.f}
 { /* DUMMY BODY */ }
 
 void TimeSystem::update(Ogre::Real delta)
@@ -9,7 +9,7 @@ void TimeSystem::update(Ogre::Real delta)
 	for(auto& ent : entities_.get_component_container<TimeComponent>())
 	{
 		if(ent.second.curr_time < ent.second.time_limit)
-			ent.second.curr_time += delta;
+			ent.second.curr_time += delta * time_multiplier_;
 		else
 			handle_event_(ent.first, ent.second);
 	}
@@ -19,6 +19,25 @@ void TimeSystem::advance_all_timers(Ogre::Real delta)
 {
 	for(auto& ent : entities_.get_component_container<TimeComponent>())
 		ent.second.curr_time += delta;
+}
+
+void TimeSystem::advance_all_timers_of_type(Ogre::Real delta, TIME_EVENT type)
+{
+	for(auto& ent : entities_.get_component_container<TimeComponent>())
+	{
+		if(ent.second.event_type == type)
+			ent.second.curr_time += delta;
+	}
+}
+
+void TimeSystem::set_time_multiplier(Ogre::Real val)
+{
+	time_multiplier_ = val;
+}
+
+Ogre::Real TimeSystem::get_time_multiplier()
+{
+	return time_multiplier_;
 }
 
 void TimeSystem::handle_event_(std::size_t id, TimeComponent& comp)
--- TimeSystem.hpp
+++ TimeSystem.hpp
@@ -31,6 +31,27 @@ class TimeSystem : public System
 		 * Param: Time to add.
 		 */
 		void advance_all_timers(Ogre::Real);
+
+		/**
+		 * Brief: Adds a given time value to all TimeComponents that match
+		 *        the given time event type.
+		 * Param: Time to add.
+		 * Param: Time even type to match.
+		 */
+		void advance_all_timers_of_type(Ogre::Real, TIME_EVENT);
+
+		/**
+		 * Brief: Sets the time value by which are all frame times multiplied
+		 *        when added to timers (this allows to slow/speed up all timers).
+		 * Param: The new time multiplier.
+		 */
+		void set_time_multiplier(Ogre::Real = 1.f);
+
+		/**
+		 * Brief: Returns the time value by which are all frame times multiplied
+		 *        when added to timers.
+		 */
+		Ogre::Real get_time_multiplier();
 	private:
 		/**
 		 * Brief: Handles a time event when it's timer finnishes.
@@ -43,4 +64,9 @@ class TimeSystem : public System
 		 * Reference to the game's entity system.
 		 */
 		EntitySystem& entities_;
+
+		/**
+		 * Allows to speed up all timers.
+		 */
+		Ogre::Real time_multiplier_;
 };
