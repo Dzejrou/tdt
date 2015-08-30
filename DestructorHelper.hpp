029eca0e0c731d9a749f6da304c73d4a1b0db03b
dzejrou <dzejrou@gmail.com>
2015-08-30 14:49:58 +0200

    added documenting comments

--- DestructorHelper.hpp
+++ DestructorHelper.hpp
@@ -7,9 +7,27 @@ class EntitySystem;
 
 namespace DestructorHelper
 {
+	/**
+	 * Brief: Sets the name of the table that contains the "dtor" function
+	 *        which get's called when a given entity is destroyed.
+	 * Param: EntitySystem containing the entity.
+	 * Param: ID of the entity.
+	 * Param: The new blueprint table's name.
+	 */
 	void set_blueprint(EntitySystem&, std::size_t, const std::string&);
 
+	/**
+	 * Brief: Returns the name of the table that contains the "dtor" function
+	 *        which get's called when a given entity is destroyed.
+	 * Param: EntitySystem containing the entity.
+	 * Param: ID of the entity.
+	 */
 	const std::string& get_blueprint(EntitySystem&, std::size_t);
 
+	/**
+	 * Brief: Destroys a given entity and if possible calls it's destructor.
+	 * Param: EntitySystem containing the entity.
+	 * Param: ID of the entity.
+	 */
 	void destroy(EntitySystem&, std::size_t);
 }
