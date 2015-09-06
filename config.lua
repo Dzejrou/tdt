c3d89f1f7a5fdf051042798607f75d2e4a6d5465
dzejrou <dzejrou@gmail.com>
2015-09-06 18:43:57 +0200

    created the main config file, so far sets all periods & multipliers and contains the script directory
    the script directory variable will allow the programmer/user to have
    different script directories for different uses of the game without the
    need to move any files

---
+++ config.lua
@@ -0,0 +1,19 @@
+game = game or {}
+
+-- GAME CONFIGURATION
+game.config = {
+	-- GENERAL
+	script_directory = "scripts",
+
+	-- TIME PERIODS & MULTIPLIERS
+	ai_update_period = 0.5,
+	production_multiplier = 1.0,
+	timer_multiplier = 1.0,
+	event_update_multiplier = 1.0
+}
+
+-- TODO: Autoset of the config properties.
+game.set_update_period(game.config.ai_update_period)
+game.set_production_multiplier(game.config.production_multiplier)
+game.set_timer_multiplier(game.config.timer_multiplier)
+game.set_event_update_multiplier(game.config.event_update_multiplier)
