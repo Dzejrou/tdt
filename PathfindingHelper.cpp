95c46469babb37a0c042c85c26b1c1bfc2613ecd
dzejrou <dzejrou@gmail.com>
2015-09-07 16:17:52 +0200

    added the ability to pop from both ends of the path queue

--- PathfindingHelper.cpp
+++ PathfindingHelper.cpp
@@ -19,3 +19,14 @@ void PathfindingHelper::set_pathfinding_blueprint(EntitySystem& ents, std::size_
 	if(comp)
 		comp->blueprint = blueprint;
 }
+
+std::deque<std::size_t>& PathfindingHelper::get_path(EntitySystem& ents, std::size_t id)
+{
+	static std::deque<std::size_t> NO_PATH{};
+
+	auto comp = ents.get_component<PathfindingComponent>(id);
+	if(comp)
+		return comp->path_queue;
+	else
+		return NO_PATH;
+}
--- PathfindingHelper.hpp
+++ PathfindingHelper.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <cstdlib>
+#include <deque>
 class EntitySystem;
 
 namespace PathfindingHelper
@@ -9,14 +10,23 @@ namespace PathfindingHelper
 	/**
 	 * Brief: Returns constant reference to the pathfinding blueprint of a given entity.
 	 *        (Which is used for the can_pass & can_break methods in lua.)
+	 * Param: EntitySystem containing the entity.
 	 * Param: ID of the entity.
 	 */
 	const std::string& get_pathpfinding_blueprint(EntitySystem&, std::size_t);
 
 	/**
 	 * Brief: Changes the pathfinding blueprint of a given entity.
+	 * Param: EntitySystem containing the entity.
 	 * Param: ID of the entity.
 	 * Param: Name of the new blueprint table.
 	 */
 	void set_pathfinding_blueprint(EntitySystem&, std::size_t, const std::string&);
+
+	/**
+	 * Brief: Returns the node queue of a given entity's path.
+	 * Param: EntitySystem containing the entity.
+	 * Param: ID of the entity.
+	 */
+	std::deque<std::size_t>& get_path(EntitySystem&, std::size_t);
 }
