686e671b7f6818e0a6eebd956b38039a7aaf9ced
dzejrou <dzejrou@gmail.com>
2015-09-11 20:14:00 +0200

    removed currently unused method, if added, it should go to the PathfindingHelper namespace

--- GridSystem.hpp
+++ GridSystem.hpp
@@ -152,12 +152,6 @@ class GridSystem : public System
 		std::size_t get_manhattan_distance(std::size_t, std::size_t) const;
 
 		/**
-
-		/**
-		 * TODO: Implement this mechanic.
-		 */
-		bool can_pass(std::size_t, std::size_t) const;
-
 		/**
 		 * Brief: Returns the board relative coordinates (row & column)
 		 *        of a given node.
