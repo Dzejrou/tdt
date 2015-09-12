f18c4218acbbb251878ef3891a763fd082926dd6
dzejrou <dzejrou@gmail.com>
2015-09-12 19:46:02 +0200

    implemented the Grid class

--- Grid.cpp
+++ Grid.cpp
@@ -25,19 +25,30 @@ void Grid::clear_unfreed()
 	unfreed_.clear();
 }
 
-void Grid::add_node(std::size_t id)
+std::size_t Grid::add_node(EntitySystem& ents, Ogre::Vector2 pos)
 {
-	nodes_.push_back(id);
+	if(nodes_.size() < width_ * height_)
+	{
+		auto id = ents.create_entity();
+		ents.add_component<GridNodeComponent>(id);
+		PhysicsHelper::set_2d_position(ents, id, pos);
+
+		return id;
+	}
+	else
+		return Component::NO_ENTITY;
 }
 
 void Grid::add_freed(std::size_t id)
 {
-	freed_.push_back(id);
+	if(in_board(id))
+		freed_.push_back(id);
 }
 
 void Grid::add_unfreed(std::size_t id)
 {
-	unfreed_.push_back(id);
+	if(in_board(id))
+		unfreed_.push_back(id);
 }
 
 void Grid::remove_node(std::size_t id)
@@ -45,9 +56,102 @@ void Grid::remove_node(std::size_t id)
 	std::remove(nodes_.begin(), nodes_.end(), id);
 }
 
+std::size_t Grid::get_node(std::size_t x, std::size_t y) const
+{
+	if(in_board(x + y * width_))
+		return nodes_[x + y * width_];
+	else
+		return Component::NO_ENTITY;
+}
+
+std::size_t Grid::get_node_from_position(Ogre::Real x, Ogre::Real y) const
+{
+	x = (x - start_.x) / distance_;
+	y = (y - start_.y) / distance_;
+	std::size_t res_x = (std::size_t)x;
+	std::size_t res_y = (std::size_t)y;
+	Ogre::Real off_x = x - res_x;
+	Ogre::Real off_y = y - res_y;
+
+	// Finds the closest.
+	if(off_x > distance_ / 200)
+		++res_x;
+	if(off_y > distance_ / 200)
+		++res_y;
+
+	return get_node(res_x, res_y);
+}
+
+void Grid::create_graph(EntitySystem& ents, Ogre::Vector2 start, std::size_t w, std::size_t h, Ogre::Real d)
+{
+	for(const auto& node : nodes_)
+		DestructorHelper::destroy(ents, node, true);
+	nodes_.clear();
+	nodes_.reserve(w * h);
+	ents.cleanup(); // Create special cleanup only for entities with a given component?
+
+	start_ = start;
+	width_ = w;
+	height_ = h;
+	distance_ = d;
+	starting_index_ = Component::NO_ENTITY;
+
+	std::vector<GridNodeComponent*> comps(width_ * height_); // Keep pointers to components for fast access.
+
+	Ogre::Vector2 pos{start_};
+	auto node_count = w * h;
+	for(std::size_t i = 0; i < node_count; ++i)
+	{
+		pos.x = (i % width_) * distance_;
+		pos.y = (i / height_) * distance_;
+		auto id = add_node(ents, pos);
+		auto comp = ents.get_component<GridNodeComponent>(id);
+		if(comp) // Should never be false though as the component is added 2 lines above...
+			comps[i] = comp;
+		comps[i]->x = i % width_;
+		comps[i]->y = i / height_;
+	}
+
+	// Link nodes.
+	for(std::size_t i = 0; i < node_count; ++i)
+		link_(i, comps);
+}
+
 Grid& Grid::instance()
 {
 	static Grid inst{};
 
 	return inst;
+}
+
+void Grid::link_(std::size_t index, std::vector<GridNodeComponent*>& comps)
+{
+	if(!in_board(index) || !comps[index])
+		return;
+	std::size_t x{comps[index]->x}, y{comps[index]->y};
+	bool bottom{y == height_ - 1}, top{y == 0}, left{x == 0}, right = {x == width_ - 1};
+
+	if(!right && comps[index + 1])
+		comps[index]->neighbours[DIRECTION::RIGHT] = index + 1;
+
+	if(!left && comps[index - 1])
+		comps[index]->neighbours[DIRECTION::LEFT] = index - 1;
+
+	if(!bottom && comps[index + width_])
+		comps[index]->neighbours[DIRECTION::DOWN] = index + width_;
+
+	if(!top && comps[index - width_])
+		comps[index]->neighbours[DIRECTION::UP] = index - width_;
+
+	if(!bottom && !left && comps[index + width_ - 1])
+		comps[index]->neighbours[DIRECTION::DOWN_LEFT] = index + width_ - 1;
+
+	if(!top && !left && comps[index - width_ - 1])
+		comps[index]->neighbours[DIRECTION::UP_LEFT] = index - width_ - 1;
+
+	if(!bottom && !right && comps[index + width_ + 1])
+		comps[index]->neighbours[DIRECTION::DOWN_RIGHT] = index + width_ + 1;
+
+	if(!top && !right && comps[index - width_ + 1])
+		comps[index]->neighbours[DIRECTION::UP_RIGHT] = index - width_ + 1;
 }
--- Grid.hpp
+++ Grid.hpp
@@ -5,32 +5,172 @@
 #include <vector>
 #include <algorithm>
 #include "EntitySystem.hpp"
+#include "Components.hpp"
+#include "Helpers.hpp"
+#include "Enums.hpp"
 
+/**
+ * Class representing the pathfinding grid.
+ */
 class Grid
 {
+	/**
+	 * GameSerializer is a friend class so that it can easily access the grid realted data
+	 * (like dimensions and node distance) when saving the game.
+	 */
+	friend class GameSerializer;
 	public:
+		/**
+		 * Brief: Returns true if a given node is in the grid.
+		 * Param: ID of the node.
+		 */
 		bool in_board(std::size_t) const;
+
+		/**
+		 * Brief: Returns a constant reference to the list of
+		 *        freed nodes.
+		 */
 		const std::vector<std::size_t>& get_freed() const;
+
+		/**
+		 * Brief: Returns a constant reference to the list of
+		 *        unfreed nodes.
+		 */
 		const std::vector<std::size_t>& get_unfreed() const;
+
+		/**
+		 * Brief: Removes all nodes from the list of freed nodes.
+		 */
 		void clear_freed();
+
+		/**
+		 * Brief: Removes all nodes from the list of unfreed nodes.
+		 */
 		void clear_unfreed();
 
-		void add_node(std::size_t);
+		/**
+		 * Brief: Created a new node at the given position.
+		 * Param: EntitySystem that contains the node.
+		 * Param: 2D position of the node.
+		 */
+		std::size_t add_node(EntitySystem&, Ogre::Vector2);
+
+		/**
+		 * Brief: Adds a given node to the list of the freed nodes.
+		 * Param: ID of the node.
+		 */
 		void add_freed(std::size_t);
+
+		/**
+		 * Brief: Adds a given node to the list of the unfreed nodes.
+		 * Param: ID of the node.
+		 */
 		void add_unfreed(std::size_t);
+
+		/**
+		 * Brief: Removes a given node from the node list.
+		 * Param: ID of the node.
+		 * TODO: Possibly implement "unlink"?
+		 */
 		void remove_node(std::size_t);
 
-		void create_graph(EntitySystem&, std::size_t, std::size_t, Ogre::Real);
+		/**
+		 * Brief: Returns the ID of a node at a given position in the grid.
+		 * Param: Column number.
+		 * Param: Row number.
+		 */
+		std::size_t get_node(std::size_t, std::size_t) const;
+		
+		/**
+		 * Brief: Returns the ID of a node that is closed to a given world coorinate.
+		 * Param: X axis coordinate.
+		 * Param: Z axis coordinate.
+		 * Note: Adding the ability to specify in what direction the node must be might
+		 *       be beneficial for pathfinding.
+		 */
+		std::size_t get_node_from_position(Ogre::Real, Ogre::Real) const;
 
+		/**
+		 * Brief: Generates a grid graph with the given parameters to be used for
+		 *        pathfinding.
+		 * Param: EntitySystem that will contain the nodes.
+		 * Param: Starting position (x,z axes) of the grid. 
+		 * Param: Width of the graph (in node count).
+		 * Param: Height of the graph (in node count).
+		 * Param: Distance between adjascent nodes.
+		 */
+		void create_graph(EntitySystem&, Ogre::Vector2, std::size_t, std::size_t, Ogre::Real);
+
+		/**
+		 * Brief: Returns a reference to the static instance of this class.
+		 * Note: Handles initialization and safe destruction by itself.
+		 * TODO: Possibly unite this an Lpp::Script under one singleton interface?
+		 */
 		static Grid& instance();
 	private:
+		/**
+		 * Constructor. 
+		 * Kept private since there should be only one grid at all times.
+		 */
 		Grid() = default;
+
+		/**
+		 * Destructor.
+		 */
 		~Grid() {}
+
+		/**
+		 * Since there should be only one grid at all times accesible from the Grid::instance
+		 * method, all copy/move operations are disabled for this class.
+		 */
 		Grid(const Grid&) = delete;
 		Grid& operator=(const Grid&) = delete;
 		Grid(Grid&&) = delete;
 		Grid& operator=(Grid&&) = delete;
 
-		std::vector<std::size_t> nodes_, freed_, unfreed_;
+		/**
+		 * Brief: Generates a neighbour list for a given node (thus linking it to the graph).
+		 * Param: ID of the node.
+		 * Param: Auxuliary vector containing component pointers for fast access.
+		 *       (This method will ever be called only in the GridSystem::create_graph method,
+		 *        which already has such a vector and so it's used here too.)
+		 */
+		void link_(std::size_t, std::vector<GridNodeComponent*>&);
+
+		/**
+		 * Vector containing the IDs of the nodes in the grid, basically
+		 * representing a 2D matrix stored in a 1D container.
+		 */
+		std::vector<std::size_t> nodes_;
+
+		/**
+		 * Auxiliary vectors containing IDs of the nodes that have been
+		 * freed/unfreed on last frame. Used for pathfinding correction
+		 * and structure model changes.
+		 */
+		std::vector<std::size_t> freed_, unfreed_;
+
+		/**
+		 * Dimensions of the grid in node count.
+		 * (Actual dimensions = dimensions * distance.)
+		 */
 		std::size_t width_, height_;
+
+		/**
+		 * Distance between two adjascent nodes.
+		 */
+		Ogre::Real distance_;
+
+		/**
+		 * ID of the first node, this allows for the node IDs to be outside the
+		 * (0, width_ * height_) range,
+		 */
+		std::size_t starting_index_;
+
+		/**
+		 * Coordinates of the starting node of the grid.
+		 * (This 2D vector contains X and Z coordinates despite it's second member
+		 *  being names Y).
+		 */
+		Ogre::Vector2 start_;
 };
