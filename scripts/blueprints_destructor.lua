b437df154bb8c0e24cee9044a603438e19cab0bd
dzejrou <dzejrou@gmail.com>
2015-09-20 15:28:28 +0200

    fixed a problem where gold pile would crash the game after the gold dropping constructor was called
    that was because the gold_pile *GLOBAL* variable got overriden! next
    time declare everything local plz

--- scripts/blueprints_destructor.lua
+++ scripts/blueprints_destructor.lua
@@ -6,16 +6,16 @@ drop_gold_destructor = {
 	dtor = function(id, killer)
 		-- TODO: Add experience to the killer?
 
-		gold_pile = game.create_entity("gold_pile")
-		game.set_curr_gold(gold_pile, game.get_curr_gold(id))
-		game.set_max_gold(gold_pile, game.get_max_gold(id))
-		x1, y1, z1 = game.get_position(gold_pile)
-		x2, y2, z2 = game.get_position(id)
-		game.move_to(gold_pile, x2, y1, z2)
+		local pile = game.create_entity("gold_pile")
+		game.set_curr_gold(pile, game.get_curr_gold(id))
+		game.set_max_gold(pile, game.get_max_gold(id))
+		local x1, y1, z1 = game.get_position(pile)
+		local x2, y2, z2 = game.get_position(id)
+		game.move_to(pile, x2, y1, z2)
 
-		evt = game.create_entity("")
+		local evt = game.create_entity("")
 		game.add_component(evt, game.enum.component.event)
-		game.set_event_target(evt, gold_pile)
+		game.set_event_target(evt, pile)
 		game.set_event_type(evt, game.enum.event.gold_dropped)
 		game.set_event_radius(evt, 300.0)
 
