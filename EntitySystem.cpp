7d676ee64107f9b5d74c17c842eabacc0674523f
dzejrou <dzejrou@gmail.com>
2015-10-24 16:38:24 +0200

    added a documenting comment

--- EntitySystem.cpp
+++ EntitySystem.cpp
@@ -8,6 +8,11 @@
  */
 #define LOAD_COMPONENT(TYPE, ID, TABLE) ((this)->*loaders_[TYPE])(ID, TABLE)
 
+/**
+ * Brief: Macro that serves as a simplerr way to use the adders_ array when calling it's members.
+ * Param: Type of the component to be added.
+ * Param: ID of the entity that gets the new component.
+ */
 #define ADD_COMPONENT(TYPE, ID) ((this)->*adders_[TYPE])(ID)
 
 EntitySystem::EntitySystem(Ogre::SceneManager& mgr)
