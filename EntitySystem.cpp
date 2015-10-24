976f38097a156ec7adcb4eeb52a22a3517af22f4
dzejrou <dzejrou@gmail.com>
2015-10-24 16:13:03 +0200

    added an auxiliary macro that allows simple loader_ element calls
    usage: LOAD_COMPONENT(TYPE OF THE COMPONENT, ID OF THE ENTITY, NAME OF
    THE TABLE TO BE LOADED)

--- EntitySystem.cpp
+++ EntitySystem.cpp
@@ -1,5 +1,7 @@
 #include "EntitySystem.hpp"
 
+#define LOAD_COMPONENT(TYPE, ID, TABLE) ((this)->*loaders_[TYPE])(ID, TABLE)
+
 EntitySystem::EntitySystem(Ogre::SceneManager& mgr)
 	: scene_{mgr}, entities_{}, to_be_destroyed_{},
 	  components_to_be_removed_{}, entity_register_{}
