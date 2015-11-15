210e8615fca7c17b94424a4e0b348ad52dba8642
dzejrou <dzejrou@gmail.com>
2015-11-15 22:02:37 +0100

    formatting

--- InputHelper.cpp
+++ InputHelper.cpp
@@ -9,7 +9,7 @@ void InputHelper::set_input_handler(EntitySystem& ents, std::size_t id, const st
 		comp->input_handler = handler;
 }
 
-const std::string & InputHelper::get_input_handler(EntitySystem& ents, std::size_t id)
+const std::string& InputHelper::get_input_handler(EntitySystem& ents, std::size_t id)
 {
 	static const std::string NO_HANDLER{"ERROR"};
 	auto comp = ents.get_component<InputComponent>(id);
