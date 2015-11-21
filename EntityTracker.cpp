5e3b4fc5dd435efea5ad79c63f07e992213f2a6e
dzejrou <dzejrou@gmail.com>
2015-11-21 22:53:04 +0100

    created the entity tracker

---
+++ EntityTracker.cpp
@@ -0,0 +1,55 @@
+#include "EntityTracker.hpp"
+#include "Components.hpp"
+
+EntityTracker::EntityTracker()
+	: curr_tracked_entity_{Component::NO_ENTITY}
+{ /* DUMMY BODY */ }
+
+void EntityTracker::set_tracked_entity(std::size_t id, EntitySystem& ents)
+{
+	curr_tracked_entity_ = id;
+	auto view = window_->getChild("FRAME");
+	if(id == Component::NO_ENTITY)
+		clear();
+	else
+	{
+		view->getChild("ID_VALUE")->setText(std::to_string(id));
+		auto health = ents.get_component<HealthComponent>(id);
+		if(health)
+			update_tracking("HP_VALUE", std::to_string(health->curr_hp) + " / " + std::to_string(health->max_hp));
+		
+		// TODO: MANA
+		// TODO: EXP
+		// TODO: LVL
+
+		auto gold = ents.get_component<GoldComponent>(id);
+		if(gold)
+			update_tracking("GOLD_VALUE", std::to_string(gold->curr_amount) + " / " + std::to_string(gold->max_amount));
+	}
+}
+
+std::size_t EntityTracker::get_tracked_entity() const
+{
+	return curr_tracked_entity_;
+}
+
+void EntityTracker::update_tracking(const std::string& label, const std::string& value)
+{
+	window_->getChild("FRAME/" + label)->setText(value);
+}
+
+void EntityTracker::clear()
+{
+	auto view = window_->getChild("FRAME");
+	view->getChild("ID_VALUE")->setText("NONE");
+	view->getChild("HP_VALUE")->setText("0 / 0");
+	view->getChild("MANA_VALUE")->setText("0 / 0");
+	view->getChild("EXP_VALUE")->setText("0 / 0");
+	view->getChild("LVL_VALUE")->setText("0");
+	view->getChild("GOLD_VALUE")->setText("0 / 0");
+}
+
+void EntityTracker::init_()
+{
+	clear();
+}
---
+++ EntityTracker.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "EntitySystem.hpp"
+#include "GUIWindow.hpp"
+
+/**
+ *
+ */
+class EntityTracker : public GUIWindow
+{
+	public:
+		/**
+		 * Constructor.
+		 */
+		EntityTracker();
+
+		/**
+		 * Destructor.
+		 */
+		~EntityTracker() {}
+
+		/**
+		 * Brief: Sets the new tracked entity and loads it's data.
+		 * Param: ID of the entity.
+		 * Param: EntitySystem that contains the entity.
+		 */
+		void set_tracked_entity(std::size_t, EntitySystem&);
+
+		/**
+		 * Brief: Returns the ID of the currently tracked entity.
+		 */
+		std::size_t get_tracked_entity() const;
+
+		/**
+		 * Brief: Updates a single stat of the entity tracker.
+		 * Param: Name of the stat label (e.g. "HP_LABEL", "GOLD_LABEL" etc).
+		 * Param: String with the new value.
+		 * Note: The value should have the form "[CURRENT_VALUE]/[MAX_VALUE]".
+		 */
+		void update_tracking(const std::string&, const std::string&);
+
+		/**
+		 * Brief: Clears the entity tracker's window, that is sets all
+		 *        values to 0/0 and the id to UNKNOWN.
+		 */
+		void clear();
+	protected:
+		/**
+		 * Brief: Initializes the window and sets all event subscribers.
+		 */
+		void init_();
+	private:
+		/**
+		 * ID of the currently tracked entity.
+		 */
+		std::size_t curr_tracked_entity_;
+};
