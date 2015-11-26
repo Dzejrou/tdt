adc27305c4ec6dc1c87f3df7ac00bdd2ff5144ef
dzejrou <dzejrou@gmail.com>
2015-11-26 23:04:08 +0100

    created a mine which spawns ogre miners

---
+++ scripts/mine.lua
@@ -0,0 +1,48 @@
+mine = {
+	components = {
+		game.enum.component.physics,
+		game.enum.component.graphics,
+		game.enum.component.production,
+		game.enum.component.structure,
+		game.enum.component.faction,
+		game.enum.component.price
+	},
+
+	PhysicsComponent = {
+		solid = false
+	},
+
+	GraphicsComponent = {
+		mesh = "cube.mesh",
+		material = "colour/brown",
+		visible = true,
+		manual_scaling = true,
+		scale_x = 50,
+		scale_y = 1,
+		scale_z = 50,
+		query_flags = game.enum.entity_type.building
+	},
+
+	ProductionComponent = {
+		blueprint = "ogre_miner",
+		limit = 1,
+		cooldown = 5
+	},
+
+	StructureComponent = {
+		radius = 0,
+		walk_through = true
+	},
+
+	FactionComponent = {
+		faction = game.enum.faction.friendly
+	},
+
+	PriceComponent = {
+		price = 400
+	},
+}
+if game then
+	game.entity.register("mine")
+	game.gui.builder.register_building("mine")
+end
