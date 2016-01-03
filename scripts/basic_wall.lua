ad3d6e55bee79b34eec1a9cd9a2e182c3d23a54e
Jaroslav Jindrak <dzejrou@gmail.com>
2016-01-03 17:49:54 +0100

    added alignment component

--- scripts/basic_wall.lua
+++ scripts/basic_wall.lua
@@ -3,7 +3,8 @@ basic_wall = {
 		game.enum.component.physics,
 		game.enum.component.health,
 		game.enum.component.graphics,
-		game.enum.component.structure
+		game.enum.component.structure,
+		game.enum.component.align
 	},
 
 	PhysicsComponent = {
@@ -21,14 +22,72 @@ basic_wall = {
 		material = "colour/brown",
 		visible = true,
 		manual_scaling = true,
-		scale_x = 50,
-		scale_y = 50,
-		scale_z = 50,
-		query_flags = game.enum.entity_type.wall
+		scale_x = 50.0,
+		scale_y = 50.0,
+		scale_z = 50.0,
+		query_flags = game.enum.entity_type.wall,
 	},
 
 	StructureComponent = {
 		radius = 0
+	},
+
+	AlignComponent = {
+		state_0 = {
+			material = "colour/brown",
+			mesh = "cube.mesh",
+			position_offset_x = 0.0,
+			position_offset_y = 0.0,
+			position_offset_z = 0.0,
+			scale_x = 25.0,
+			scale_y = 50.0,
+			scale_z = 25.0
+		},
+
+		state_1 = {
+			material = "colour/brown",
+			mesh = "cube.mesh",
+			position_offset_x = 0.0,
+			position_offset_y = 0.0,
+			position_offset_z = 0.0,
+			scale_x = 25.0,
+			scale_y = 50.0,
+			scale_z = 50.0
+		},
+
+		state_2 = {
+			material = "colour/brown",
+			mesh = "half_cube.mesh",
+			position_offset_x = 0.0,
+			position_offset_y = 0.0,
+			position_offset_z = 0.0,
+			scale_x = 50.0,
+			scale_y = 50.0,
+			scale_z = 50.0
+		},
+
+		state_3 = {
+			material = "colour/brown",
+			mesh = "cube.mesh",
+			position_offset_x = 0.0,
+			position_offset_y = 0.0,
+			position_offset_z = 0.0,
+			scale_x = 50.0,
+			scale_y = 50.0,
+			scale_z = 50.0
+		},
+
+		state_4 = {
+			material = "colour/brown",
+			mesh = "cube.mesh",
+			position_offset_x = 0.0,
+			position_offset_y = 0.0,
+			position_offset_z = 0.0,
+			scale_x = 50.0,
+			scale_y = 50.0,
+			scale_z = 50.0
+		}
+
 	}
 }
 
