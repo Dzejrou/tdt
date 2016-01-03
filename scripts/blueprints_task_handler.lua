69747adfb461bc627dda24b4203f825af0e2b760
Jaroslav Jindrak <dzejrou@gmail.com>
2016-01-03 21:07:32 +0100

    added a TODO

--- scripts/blueprints_task_handler.lua
+++ scripts/blueprints_task_handler.lua
@@ -43,6 +43,7 @@ default_task_handler = {
 		elseif task_type == game.enum.task.pick_up_gold then
 			game.gold.transfer_all(target, id)
 			if game.gold.get_current(target) > 0 then
+				-- TODO: Let others pick it up.
 				game.gui.log.print("\\[#" .. id .. "\\] My sack is full!")
 				evt = game.entity.create("")
 				game.entity.add_component(evt, game.enum.component.event)
