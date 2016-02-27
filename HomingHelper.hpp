1a0c88dd000f198e76ade9992bb64c99b5f33a84
Jaroslav Jindrak <dzejrou@gmail.com>
2016-02-27 19:20:54 +0100

    added summary comment to the namespace

--- HomingHelper.hpp
+++ HomingHelper.hpp
@@ -3,6 +3,10 @@
 #include <cstdlib>
 class EntitySystem;
 
+/**
+ * Auxiliary namespace containing functions that help with the management of
+ * the homing component.
+ */
 namespace HomingHelper
 {
 	/**
