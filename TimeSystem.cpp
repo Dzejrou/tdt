ac15715d4914cdff1247349a8118e030500efa22
Jaroslav Jindrak <dzejrou@gmail.com>
2016-02-27 19:45:53 +0100

    added the new components with cooldowns to the time system update

--- TimeSystem.cpp
+++ TimeSystem.cpp
@@ -13,10 +13,36 @@ void TimeSystem::update(Ogre::Real delta)
 		else
 			handle_event_(ent.first, ent.second);
 	}
+
+	for(auto& ent : entities_.get_component_container<OnHitComponent>())
+	{
+		if(ent.second.curr_time < ent.second.cooldown)
+			ent.second.curr_time += delta * time_multiplier_;
+	}
+
+	for(auto& ent : entities_.get_component_container<TriggerComponent>())
+	{
+		if(ent.second.curr_time < ent.second.cooldown)
+			ent.second.curr_time += delta * time_multiplier_;
+	}
+
+	for(auto& ent : entities_.get_component_container<NotificationComponent>())
+	{
+		if(ent.second.curr_time < ent.second.cooldown)
+			ent.second.curr_time += delta * time_multiplier_;
+	}
+
+	for(auto& ent : entities_.get_component_container<LimitedLifeSpanComponent>())
+	{
+		if(ent.second.curr_time < ent.second.max_time)
+			ent.second.curr_time += delta * time_multiplier_;
+		else
+			DestructorHelper::destroy(entities_, ent.first);
+	}
 }
 
 void TimeSystem::advance_all_timers(Ogre::Real delta)
-{
+{ // Note: Timers refers only to TimeComponents, ignore the others.
 	for(auto& ent : entities_.get_component_container<TimeComponent>())
 		ent.second.curr_time += delta;
 }
