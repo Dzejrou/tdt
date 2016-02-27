5c723a6cf0b20c630fd07b3022d743ceccd240d3
Jaroslav Jindrak <dzejrou@gmail.com>
2016-02-27 19:44:35 +0100

    created a new walkway entity, which is walkthrough floor that prevents buildings from being placed on it
    this will be used from the enemy spawner to the center of the level?

---
+++ scripts/walkway.lua
@@ -0,0 +1,31 @@
+walkway = {
+	components = {
+		game.enum.component.physics,
+		game.enum.component.graphics,
+		game.enum.component.structure,
+	},
+
+	PhysicsComponent = {
+		solid = false
+	},
+
+	GraphicsComponent = {
+		mesh = "cube.mesh",
+		material = "colour/gray",
+		visible = true,
+		manual_scaling = true,
+		scale_x = 50.0,
+		scale_y = 0.2,
+		scale_z = 50.0,
+		query_flags = game.enum.entity_type.misc, -- Not selectable.
+	},
+
+	StructureComponent = {
+		radius = 0,
+		walk_through = true
+	},
+}
+
+if game then
+	game.entity.register("walkway")
+end
