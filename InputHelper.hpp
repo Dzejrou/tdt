8dcfe9464a520f39cba6a1af9023d3a236fcc1d5
Jaroslav Jindrak <dzejrou@gmail.com>
2016-02-27 19:21:01 +0100

    added summary comment to the namespace

--- InputHelper.hpp
+++ InputHelper.hpp
@@ -4,6 +4,10 @@
 #include <cstdlib>
 class EntitySystem;
 
+/**
+ * Auxiliary namespace containing functions that help with the management of
+ * the input component.
+ */
 namespace InputHelper
 {
 	/**
