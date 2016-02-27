6b22d334b86afa680e7b8588a4c6e8710c312f5a
Jaroslav Jindrak <dzejrou@gmail.com>
2016-02-27 19:27:42 +0100

    added a couple of new settings
    new period times, default block tables for level generations etc

--- config.lua
+++ config.lua
@@ -8,7 +8,17 @@ game.config = {
 
 	-- TIME PERIODS & MULTIPLIERS
 	ai_update_period = 0.5,
+	graphics_update_period = 0.1,
 	production_multiplier = 1.0,
 	timer_multiplier = 1.0,
-	event_update_multiplier = 1.0
+	event_update_multiplier = 1.0,
+	max_run_away_attempts = 50,
+	trigger_check_period = 1.0,
+	mana_regen_period = 5.0,
+
+	-- TABLE NAMES
+	default_wall_table = "basic_wall",
+	default_gold_table = "gold_deposit",
+	default_border_table = "indestructible_wall",
+	default_walkway_table = "walkway"
 }
