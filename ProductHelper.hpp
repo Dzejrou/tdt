c338d1819c4bbb32d92e864bd384c6bff74ba4c1
Jaroslav Jindrak <dzejrou@gmail.com>
2016-02-27 19:22:21 +0100

    added summary comment to the namespace

--- ProductHelper.hpp
+++ ProductHelper.hpp
@@ -3,6 +3,10 @@
 #include <cstdlib>
 class EntitySystem;
 
+/**
+ * Auxiliary namespace containing functions that help with the management of the
+ * product component.
+ */
 namespace ProductHelper
 {
 	/**
