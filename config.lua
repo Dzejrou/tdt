29112ab430ca26944ac5e617ce34b5c4e2477912
Jaroslav Jindrak <dzejrou@gmail.com>
2016-03-27 23:55:15 +0200

    added new config options

--- config.lua
+++ config.lua
@@ -15,10 +15,15 @@ game.config = {
 	max_run_away_attempts = 50,
 	trigger_check_period = 1.0,
 	mana_regen_period = 5.0,
+	default_wave_table = "game.wave.wave_test",
+	console_hitory = 500,
+	log_history = 50,
 
 	-- TABLE NAMES
 	default_wall_table = "basic_wall",
 	default_gold_table = "gold_deposit",
 	default_border_table = "indestructible_wall",
-	default_walkway_table = "walkway"
+	default_walkway_table = "walkway",
+	default_light_table = "light_crystal",
+	default_throne_table = "dungeon_throne"
 }
