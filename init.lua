7a26f08bc2bf5541b2a930cf8a32cf7e1e48e8f2
Jaroslav Jindrak <dzejrou@gmail.com>
2016-03-28 00:03:16 +0200

    added keybinding and new config options

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@
 -- NOTE: IT'S INADVISED TO CHANGE THE FOLLOWING CODE! -------------------------
 -------------------------------------------------------------------------------
 require("config")
+require("keybinds")
 
 -- Loads the main script of each script directory.
 for _, script_dir in ipairs(game.config.script_directories) do
@@ -18,6 +19,9 @@ game.event.set_multiplier(game.config.event_update_multiplier)
 game.combat.set_max_run_away_attempts(game.config.max_run_away_attempts)
 game.trigger.set_check_period(game.config.trigger_check_period)
 game.mana.set_regen_period(game.config.mana_regen_period)
+game.wave.set_table(game.config.default_wave_table)
+game.gui.console.set_history(game.config.console_hitory)
+game.gui.log.set_history(game.config.log_history)
 
 -- Helper commands executed on startup:
 game.toggle_camera_free_mode()
