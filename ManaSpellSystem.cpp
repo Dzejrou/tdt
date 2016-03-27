64642a9b11325583912691845f4dee6678d4fc06
Jaroslav Jindrak <dzejrou@gmail.com>
2016-03-28 00:05:03 +0200

    finnished mana spell system

--- ManaSpellSystem.cpp
+++ ManaSpellSystem.cpp
@@ -1,5 +1,6 @@
 #include "ManaSpellSystem.hpp"
 #include "Player.hpp"
+#include "Helpers.hpp"
 
 ManaSpellSystem::ManaSpellSystem(EntitySystem& ents)
 	: entities_{ents}, regen_timer_{}, regen_period_{5.f}
@@ -15,10 +16,24 @@ void ManaSpellSystem::update(Ogre::Real delta)
 
 		Player::instance().add_mana(Player::instance().get_mana_regen());
 
-		// TODO: Mana to entities.
+		for(auto& ent : entities_.get_component_container<ManaComponent>())
+		{
+			if(ent.second.curr_mana + ent.second.mana_regen < ent.second.max_mana)
+				ent.second.curr_mana += ent.second.mana_regen;
+			else
+				ent.second.curr_mana = ent.second.max_mana;
+		}
 	}
 
-	// TODO: Spell casting of entities.
+	for(auto& ent : entities_.get_component_container<SpellComponent>())
+	{
+		ent.second.cd_time += delta;
+		if(ent.second.cd_time >= ent.second.cooldown)
+		{
+			SpellHelper::cast(entities_, ent.first);
+			ent.second.cd_time = 0.f;
+		}
+	}
 }
 
 void ManaSpellSystem::set_regen_period(Ogre::Real val)
