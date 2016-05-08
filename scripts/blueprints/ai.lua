e80f55e5a1da3150ecc3e8f92f55fca69b733908
Jaroslav Jindrak <dzejrou@gmail.com>
2016-05-08 14:32:37 +0200

    the dumb units now attack enemies in sight rather than in range as the range can be really small and would cause them to not move

--- scripts/blueprints/ai.lua
+++ scripts/blueprints/ai.lua
@@ -23,7 +23,7 @@ smart_friendly_combat_unit_ai = {
 dumb_friendly_combat_unit_ai = {
 	update = function(id)
 		local enemy = game.combat.closest_enemy_in_sight(id)
-		if enemy ~= game.const.no_ent and game.combat.in_range(id, enemy) then
+		if enemy ~= game.const.no_ent and game.combat.in_sight(id, enemy) then
 			local task = game.task.create(enemy, game.enum.task.go_kill)
 			game.task.add(id, task)
 			return
@@ -65,7 +65,7 @@ smart_enemy_combat_unit_ai = {
 dumb_enemy_combat_unit_ai = {
 	update = function(id)
 		local enemy = game.combat.closest_enemy_in_sight(id)
-		if enemy ~= game.const.no_ent and game.combat.in_range(id, enemy) then
+		if enemy ~= game.const.no_ent and game.combat.in_sight(id, enemy) then
 			local task = game.task.create(enemy, game.enum.task.go_kill)
 			game.task.add(id, task)
 			return
